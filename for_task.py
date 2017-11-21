7473846834723cca0e318fce9f81417134573a7c
Andrey <bellistica@gmail.com>
2017-11-21 23:32:36 +0300

    Home Task part 1

---
+++ for_task.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+school_scores = [{"school_class": "4a", "scores":[3,3,5,4,2,5]},{"school_class":"4b", "scores": [2,2,4,5,3]},{"school_class":"4c", "scores":[5,5,5,4,5,5]}]
+
+for [2] in school_scores:
+    print(sc)
+
+for scores in school_scores:
+    print (np.mean(scores["scores"]))
